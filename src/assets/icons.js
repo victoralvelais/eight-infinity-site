02dc0782fda22648aa58832d88823ac72d401f1c
Eisen Uchiha <eisenuchiha@gmail.com>
2021-06-30 19:32:11 -0500

    Removed other FB icon

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Facebook from './Facebook.svg'
-import Facebook2 from './Facebook2.svg'
 import Instagram from './Instagram.svg'
 import Twitch from './Twitch.svg'
 import Twitter from './Twitter.svg'
@@ -15,7 +14,6 @@ const formatIcon = ({ color, Icon }) => {
 
 const Icons = {
   Facebook: () => formatIcon({ color: null, Icon: Facebook }),
-  Facebook2: () => formatIcon({ color: null, Icon: Facebook2 }),
   Instagram: () => formatIcon({ color: null, Icon: Instagram }),
   Twitch: () => formatIcon({ color: null, Icon: Twitch }),
   Twitter: () => formatIcon({ color: null, Icon: Twitter }),
