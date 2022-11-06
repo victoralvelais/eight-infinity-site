ed3f8f24ac301ee33870d7b2698e9c9bd86cb079
Eisen Uchiha <eisenuchiha@gmail.com>
2022-11-06 01:03:48 -0600

    remove tasks file

--- Tasks.js
+++
@@ -1,4 +0,0 @@
-// TODO Environment variables for information
-  // TODO Separate file or 3rd party source with data to pull from
-    // TODO Look into Fauna DB or a DB that I can pull data from GraphQL at build time instead of runtime
-    // Look into: https://egghead.io/courses/building-a-serverless-jamstack-todo-app-with-netlify-gatsby-graphql-and-faunadb-53bb
