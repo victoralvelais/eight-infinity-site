517e3ec6a9124d28dd5207821f6c205e23a0565e
Victor Alvelais <6127592+victoralvelais@users.noreply.github.com>
2022-11-17 22:04:06 -0600

    add music player display

---
+++ src/components/music/player.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+const MusicLink = ({ link }) => {
+  const { music, author, embeds } = link
+  const artist = embeds[0]?.author?.name || embeds[0]?.description.match(/^(.*?)·/)[1]
+  const title = embeds[0]?.title || embeds[0]?.title
+
+  return (
+    <li>
+      <a href={music}>{artist} - {title}</a>
+    </li>
+  )
+}
+
+const MusicPlayer = ({ links }) => {
+  return (
+    <section style={{ background: '#222', height: 500, padding: '10px 0' }}>
+      <ul className="music-list" style={{  }}>
+        {links.map((music, index) => (
+            <MusicLink link={music} key={index} />
+        ))}
+      </ul>
+
+      <br />
+      <section style={{ display: 'flex', justifyContent: 'center', borderRadius: 10, maxWidth: 400 }}>
+        <button style={{ background: 'red', color: 'white', width: 60, height: 40 }}>
+          |>
+        </button>
+      </section>
+    </section>
+  )
+}
+
+export default MusicPlayer
--- src/pages/music.js
+++ src/pages/music.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import MusicPlayer from "../components/music/player";
 import 'antd/dist/antd.css';
 import "./index.css"
 
@@ -25,13 +26,7 @@ const MusicPage = () => {
       <SEO />
       <section style={{ padding: 15 }}>
         <h1 style={{ textAlign: 'center' }}>Music</h1>
-        <ul style={{ maxHeight: 400 }}>
-          {music.map((music, index) => (
-            <li key={index}>
-              <a href={music}>{music}</a>
-            </li>
-          ))}
-        </ul>
+        <MusicPlayer links={music} />
       </section>
     </Layout>
   )
