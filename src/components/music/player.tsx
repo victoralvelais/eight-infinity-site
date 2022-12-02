87fc976b56ab5f1b883e6f539071ac094fb4832e
Victor Alvelais <6127592+victoralvelais@users.noreply.github.com>
2022-12-02 03:25:21 -0600

    add iframes for youtube and spotify

--- src/components/music/player.tsx
+++ src/components/music/player.tsx
@@ -1,20 +1,46 @@
 import React from "react"
+import './player.css'
+
+const insertEmbed = (link, provider) => {
+  if (provider === 'YouTube') {
+    const regex = /https?:\/\/(www\.)?(youtube\.com|youtu\.be)\/(watch\?v=|playlist\?list=)?([A-Za-z0-9\-_]+)/
+    const match = link.match(regex)
+    return `https://youtube.com/${match[3]}${match[4]}`
+  }
+
+  else {
+    const match = link.match(/https:\/\/open\.spotify\.com\/(track|album)\/(\w*)/)
+    return `https://open.spotify.com/embed/${match[1]}/${match[2]}`
+  }
+}
 
 const MusicLink = ({ link }: { link: any }) => {
   const { music, author, embeds } = link
-  const artist: string = embeds[0]?.author?.name || embeds[0]?.description?.match(/^(.*?)·/)[1] || 'Playlist'
-  const title: string = embeds[0]?.title || embeds[0]?.title
+  try {
+    const player: string = embeds[0].provider.name
+    const embedLink: string = embeds[0]?.video?.url || insertEmbed(music, player)
 
-  return (
-    <li>
-      <a href={music}>{artist} - {title}</a>
-    </li>
-  )
+    return (
+      <li className={embedLink.match(/album|playlist/) ? 'playlist' : 'track'}>
+        <iframe
+          title={`${player} Web Player`}
+          loading="lazy"
+          src={embedLink}
+          frameBorder='0'
+          allow="encrypted-media"
+          allowFullScreen
+        />
+      </li>
+    )
+  } catch(e) {
+    console.log(e, link)
+    return <li><a href={music}>Error</a></li>
+  }
 }
 
 const MusicPlayer = ({ links }) => {
   return (
-    <section style={{ background: '#222', height: 500, padding: '20px 0', borderRadius: 10, maxWidth: 400 }}>
+    <section id='music-player'>
       <ul className="music-list">
         {links.map((music, index) => <MusicLink link={music} key={index} />)}
       </ul>
