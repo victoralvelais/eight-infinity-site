6ad32a01ef40a0b22953e12d8b632a88be1238ce
Victor Alvelais <6127592+victoralvelais@users.noreply.github.com>
2022-12-09 01:14:46 -0600

    Add youtube embed component

--- src/components/music/player.tsx
+++ src/components/music/player.tsx
@@ -1,7 +1,10 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import YouTube from 'react-youtube'
 import './player.css'
 
 const insertEmbed = (link, provider) => {
+  // We should port this to the backend to create our own embed links
+  // Not sure if we will need this with the iFrame APIs
   if (provider === 'YouTube') {
     const regex = /https?:\/\/(www\.)?(youtube\.com|youtu\.be)\/(watch\?v=|playlist\?list=)?([A-Za-z0-9\-_]+)/
     const match = link.match(regex)
@@ -14,22 +17,45 @@ const insertEmbed = (link, provider) => {
   }
 }
 
-const MusicLink = ({ link }: { link: any }) => {
+const MusicLink = ({ link, setActiveTrack }: { link: any, setActiveTrack: () => void }) => {
   const { music, author, embeds } = link
-  try {
+  try { // Using try until we can configure proper embeds in the backend
+    // Some links don't have the embed information
+    // but we don't need to rely on that if we create our own embeds
     const player: string = embeds[0].provider.name
     const embedLink: string = embeds[0]?.video?.url || insertEmbed(music, player)
 
     return (
       <li className={embedLink.match(/album|playlist/) ? 'playlist' : 'track'}>
-        <iframe
-          title={`${player} Web Player`}
-          loading="lazy"
-          src={embedLink}
-          frameBorder='0'
-          allow="encrypted-media"
-          allowFullScreen
-        />
+        {player === 'YouTube' ? 
+          <YouTube // This works, but I haven't found one for Spotify
+            videoId={music.match(/watch\?v=(.*)/)[1] || 'BXPL2-MWSNY'}
+            onReady={(e) => console.log('Ready')}                   
+            onPlay={(e) => console.log('Playing')}                   
+            onPause={(e) => console.log('Paused')}                    
+            onEnd={(e) => console.log('End of song')}                     
+            onError={(e) => console.log('Error')}                    
+          /> :
+          // I think this is the right way to do it, but I'm not sure
+          // https://developer.spotify.com/documentation/embeds/guides/using-the-iframe-api/
+
+          // This is a react component for Spotify embeds
+          // But if looks limited in terms of events and controls
+          // https://github.com/ctjlewis/react-spotify-embed
+          <iframe
+            onClick={e => {
+              console.log(e)
+              setActiveTrack()
+              }
+            }
+            title={`${player} Web Player`}
+            loading="lazy"
+            src={embedLink}
+            style={{ border: 0 }}
+            allow="encrypted-media"
+            allowFullScreen
+          />
+          }
       </li>
     )
   } catch(e) {
@@ -39,16 +65,28 @@ const MusicLink = ({ link }: { link: any }) => {
 }
 
 const MusicPlayer = ({ links }) => {
+  const [activeTrack, setActiveTrack] = useState<number | boolean>(false)
+  const [playing, setPlaying] = useState<boolean>(false)
+  
+
+  useEffect(() => {
+    // This is just for testing
+    // I'm not sure if we will need useEffect
+    console.log('activeTrack', activeTrack)
+    console.log('playing', playing)
+  }, [activeTrack, playing])
+
   return (
     <section id='music-player'>
       <ul className="music-list">
-        {links.map((music, index) => <MusicLink link={music} key={index} />)}
+        {links.map((music, index) => (
+          <MusicLink link={music} key={index} setActiveTrack={() => setActiveTrack(index)} />
+        ))}
       </ul>
-
       <br />
       <section style={{ display: 'flex', justifyContent: 'center', borderRadius: 10, maxWidth: 400 }}>
         <button style={{ background: 'red', color: 'white', width: 60, height: 40 }}>
-          {'|>'}
+          {playing ? '||' : '|>'}
         </button>
       </section>
     </section>
